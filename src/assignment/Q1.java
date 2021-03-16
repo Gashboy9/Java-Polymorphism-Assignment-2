ffc016460353d8b367a9dbf323f34207f5e52b82
gashboy9 <gashboy@yahoo.com>
2021-03-16 14:35:11 +0000

    Q1 classes updated

--- src/assignment/Q1.java
+++ src/assignment/Q1.java
@@ -8,3 +8,82 @@ public class Q1 {
 	}
 
 }
+
+
+class Staff {
+	private StaffMember [] staffList;
+	
+	public void payday() {
+		
+	}
+	
+}
+
+abstract class StaffMember{
+	
+	protected String nane;
+	protected String address;
+	protected String phone;
+	
+	public String toString() {
+		return " ";
+	}
+	
+	abstract public double pay();
+	
+}
+
+
+class volunteer extends StaffMember{
+	
+	public double pay() {
+		return 0;
+	}
+	
+}
+
+
+class Employee extends StaffMember{
+	protected String socialSecurityNumber;
+	protected double payRate;
+	
+	public String toString() {
+		return " "; 
+	}
+	
+	public double pay() {
+		return 0;
+	}
+	
+}
+
+
+class Executive extends Employee{
+	private double bonus;
+	
+	public void awardBonus(double execBonus) {
+		
+	}
+	
+	public double pay() {
+		return 0;
+	}
+}
+
+
+class Hourly extends Employee{
+	private int hoursWorked;
+	
+	public void add(int moreHours) {
+		
+	}
+	
+	public double pay() {
+		return 0;
+	}
+	
+	public String toString() {
+		return " ";
+	}
+}
+
